9f875bc84357ba86205f5475a8a7578c7f156a0b
Maxim <murmax82@gmail.com>
2025-01-13 17:27:01 +0400

    run integration tests in gh actions 4

--- translationRecorder/app/src/scripts/download_chunks.py
+++ translationRecorder/app/src/scripts/download_chunks.py
@@ -20,7 +20,6 @@ EN = "en"
 OT = "ot"
 NT = "nt"
 RES = ["ulb"]
-print(DATA)
 
 OUTPUT = []
 
@@ -37,5 +36,8 @@ for x in range(0, 67):
 
     chunks_url = BASE_URL + slug + "/" + EN + "/ulb/chunks.json"
     outpath = os.path.join(ROOT_DIR, "chunks", anth, slug, "chunks.json")
+    print(outpath)
     os.makedirs(os.path.dirname(outpath), exist_ok=True)
+    print("+++++++++++++")
     urllib.request.urlretrieve(chunks_url, outpath)
+    print("-------------")
