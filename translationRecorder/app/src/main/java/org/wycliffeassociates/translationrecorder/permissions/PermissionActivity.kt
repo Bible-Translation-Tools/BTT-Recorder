b9dbbd02c32597ce0c2203b126e1a957a1421d4f
Maxim <murmax82@gmail.com>
2025-01-30 16:53:53 +0400

    use a different library for permissions

--- translationRecorder/app/src/main/java/org/wycliffeassociates/translationrecorder/permissions/PermissionActivity.kt
+++ translationRecorder/app/src/main/java/org/wycliffeassociates/translationrecorder/permissions/PermissionActivity.kt
@@ -4,21 +4,23 @@ import android.Manifest
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.permissionx.guolindev.PermissionX
+import org.wycliffeassociates.translationrecorder.R
+import org.wycliffeassociates.translationrecorder.SplashScreen
 import java.util.concurrent.atomic.AtomicBoolean
 
 abstract class PermissionActivity : AppCompatActivity() {
 
     protected val requestingPermission = AtomicBoolean(false)
 
+    private val corePermissions = mutableSetOf(
+        Manifest.permission.RECORD_AUDIO
+    )
+    private val optionalPermissions = mutableSetOf<String>()
+
     // Opens an activity to guide the user to enabling settings from the settings app
     // this is called when the user has checked "never ask again"
-    fun presentPermissionDialog() {
+    private fun presentPermissionDialog() {
         startActivity(Intent(this, PermissionsDialogActivity::class.java))
     }
 
@@ -29,53 +31,75 @@ abstract class PermissionActivity : AppCompatActivity() {
      */
     protected abstract fun onPermissionsAccepted()
 
-    override fun onResume() {
-        super.onResume()
+    override fun onStart() {
+        super.onStart()
+
         requestingPermission.set(true)
 
-        val permissions = arrayListOf(
-            Manifest.permission.RECORD_AUDIO
-        )
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            val wasDenied = ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            )
-            // Request POST_NOTIFICATIONS only once
-            if (!wasDenied) {
-                permissions.add(Manifest.permission.POST_NOTIFICATIONS)
+            if (!optionalPermissions.contains(Manifest.permission.POST_NOTIFICATIONS)) {
+                optionalPermissions.add(Manifest.permission.POST_NOTIFICATIONS)
             }
         }
 
-        Dexter.withActivity(this)
-                .withPermissions(permissions)
-                .withListener(
-                        object : MultiplePermissionsListener {
-                            override fun onPermissionsChecked(report: MultiplePermissionsReport) {
-                                if (report.areAllPermissionsGranted()) {
-                                    requestingPermission.set(false)
-                                    onPermissionsAccepted()
-                                } else {
-                                    val denied = report.deniedPermissionResponses
-                                        .map { it.permissionName }
+        val permissions = arrayListOf<String>()
+        permissions.addAll(corePermissions)
+        permissions.addAll(optionalPermissions)
 
-                                    // Skip POST_NOTIFICATIONS if the user has denied it
-                                    if (denied.size == 1 && denied.first() == Manifest.permission.POST_NOTIFICATIONS) {
-                                        requestingPermission.set(false)
-                                        onPermissionsAccepted()
-                                    } else {
-                                        requestingPermission.set(false)
-                                        presentPermissionDialog()
-                                    }
-                                }
-                            }
-                            override fun onPermissionRationaleShouldBeShown(
-                                    permissions: List<PermissionRequest>,
-                                    token: PermissionToken)
-                            {
-                                token.continuePermissionRequest()
-                            }
-                        }
-                ).check()
+        PermissionX.init(this)
+            .permissions(permissions)
+            .onForwardToSettings { scope, deniedList ->
+                val deniedCorePermissions = deniedCorePermissions(deniedList)
+                if (deniedCorePermissions.isNotEmpty()) {
+                    scope.showForwardToSettingsDialog(
+                        deniedCorePermissions,
+                        getString(R.string.permissions_denied_message),
+                        getString(R.string.open_permissions),
+                        getString(R.string.title_cancel)
+                    )
+                } else {
+                    permissionsAccepted()
+                }
+            }
+            .request { _, _, _ ->
+                val allGranted = allPermissionsGranted()
+                val deniedList = deniedPermissions()
+                when {
+                    allGranted -> permissionsAccepted()
+                    deniedOptionalPermissionsOnly(deniedList) -> permissionsAccepted()
+                    else -> restart()
+                }
+            }
+    }
+
+    private fun deniedOptionalPermissionsOnly(deniedList: List<String>): Boolean {
+        return deniedCorePermissions(deniedList).isEmpty()
+    }
+
+    private fun deniedCorePermissions(deniedList: List<String>): List<String> {
+        return deniedList.subtract(optionalPermissions).toList()
+    }
+
+    private fun restart() {
+        finishAffinity()
+        val intent = Intent(this, SplashScreen::class.java)
+        startActivity(intent)
+    }
+
+    private fun permissionsAccepted() {
+        requestingPermission.set(false)
+        onPermissionsAccepted()
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        return corePermissions.plus(optionalPermissions).all { permission ->
+            PermissionX.isGranted(this, permission)
+        }
+    }
+
+    private fun deniedPermissions(): List<String> {
+        return corePermissions.plus(optionalPermissions).filter {
+            !PermissionX.isGranted(this, it)
+        }
     }
 }
